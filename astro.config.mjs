4167595504d538ac546e0b72e97c23ee90f36238
Afham <noefeah89@gmail.com>
2024-12-04 20:48:49 +0530

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,36 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
 import sitemap from 'astro-sitemap';
-import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
-
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
+// import icon from "astro-icon";
 
 // import swup from '@swup/astro';
 
 // https://astro.build/config
 export default defineConfig({
-    prefetch: {
-        prefetchAll: false,
-        defaultStrategy: 'load',
-    },
+    // prefetch: {
+    //     prefetchAll: true,
+    //     defaultStrategy: 'load',
+    // },
 
     // },
-    site: 'https://www.petrostarinnovative.com',
-    integrations: [robotsTxt(), sitemap(), tailwind({
-        config: {
-            applyBaseStyles: false,
-        },
-    }), (await import('astro-compress')).default({
-        Image: false,
-    }), // swup({
+    site: 'https://www.creative-hive.co',
+
+    integrations: [
+        robotsTxt(),
+        sitemap(),
+        // icon(),
+        tailwind({
+            config: {
+                applyBaseStyles: false,
+            },
+        }),
+        (await import('astro-compress')).default({
+            Image: false,
+        }),
+        // swup({
         //   theme: 'fade', reloadScripts: false,
 
         //   progress: true,
@@ -31,20 +39,27 @@ export default defineConfig({
         //     hover: true,
         //   }
         // })
+        react(),
     ],
-    output: "server",
-    adapter: netlify({
-        cacheOnDemandPages: true,
-    }),
-    experimental: {
-        // serverIslands: true
-    },
+
     image: {
         remotePatterns: [
             {
                 protocol: 'https',
             },
         ],
-        domains: ['northernsky.auxcgen.com'],
+        domains: ['creativehive.auxcgen.com'],
     },
+
+    adapter: vercel({
+        imageService: true,
+        isr: {
+            expiration: 3600,
+        },
+        imagesConfig: {
+            sizes: [420, 640, 1280, 1920],
+            formats: ["image/avif"],
+            domains: ["creativehive.auxcgen.com"],
+        },
+    }),
 });
