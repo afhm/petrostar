999da4fdb0d7616b2392422c20465cc76e21d17b
Afham <noefeah89@gmail.com>
2024-12-04 19:03:49 +0530

    fix vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,46 +1,45 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
 import sitemap from 'astro-sitemap';
-import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-
-import alpinejs from '@astrojs/alpinejs';
+import vercel from '@astrojs/vercel/serverless';
+// import icon from "astro-icon";
 
 // import swup from '@swup/astro';
 
 // https://astro.build/config
 export default defineConfig({
     prefetch: {
-        prefetchAll: false,
+        prefetchAll: true,
         defaultStrategy: 'load',
     },
 
     // },
-    site: 'https://northernsky.in',
-    integrations: [robotsTxt(), sitemap(), tailwind({
-        config: {
-            applyBaseStyles: false,
-        },
-    }), (await import('astro-compress')).default({
-        Image: false,
-    }), // swup({
-    //   theme: 'fade', reloadScripts: false,
+    site: 'https://petrostarinnovative.com',
 
-    //   progress: true,
+    integrations: [
+        robotsTxt(),
+        sitemap(),
+        tailwind({
+            config: {
+                applyBaseStyles: false,
+            },
+        }),
+        (await import('astro-compress')).default({
+            Image: false,
+        }),
+        // swup({
+        //   theme: 'fade', reloadScripts: false,
 
-    //   preload: {
-    //     hover: true,
-    //   }
-    // })
-    react(), alpinejs()],
-    output: "server",
-    adapter: netlify({
-        cacheOnDemandPages: true,
-    }),
-    experimental: {
-        // serverIslands: true
-    },
+        //   progress: true,
+
+        //   preload: {
+        //     hover: true,
+        //   }
+        // })
+
+    ],
+
     image: {
         remotePatterns: [
             {
@@ -49,4 +48,15 @@ export default defineConfig({
         ],
         domains: ['northernsky.auxcgen.com'],
     },
+
+    output: "server",
+    adapter: vercel({
+        imageService: true,
+        isr: true,
+        imagesConfig: {
+            sizes: [320, 640, 1280, 1920, 2560],
+            formats: ["image/avif"],
+            domains: ["northernsky.auxcgen.com"],
+        },
+    }),
 });
