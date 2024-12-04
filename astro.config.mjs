152b8db2c2f01d103247eb6f608ef3a5bc1ed82b
Afham <noefeah89@gmail.com>
2024-12-04 20:13:50 +0530

    fix new

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,25 @@ import robotsTxt from 'astro-robots-txt';
 import sitemap from 'astro-sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
+// import icon from "astro-icon";
 
+// import swup from '@swup/astro';
+
+// https://astro.build/config
 export default defineConfig({
+    // prefetch: {
+    //     prefetchAll: true,
+    //     defaultStrategy: 'load',
+    // },
+
+    // },
     site: 'https://www.creative-hive.co',
+
     integrations: [
         robotsTxt(),
         sitemap(),
+        // icon(),
         tailwind({
             config: {
                 applyBaseStyles: false,
@@ -18,6 +30,15 @@ export default defineConfig({
         (await import('astro-compress')).default({
             Image: false,
         }),
+        // swup({
+        //   theme: 'fade', reloadScripts: false,
+
+        //   progress: true,
+
+        //   preload: {
+        //     hover: true,
+        //   }
+        // })
         react(),
     ],
 
@@ -32,9 +53,6 @@ export default defineConfig({
 
     output: 'server',
     adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
         imageService: true,
         isr: {
             expiration: 3600,
@@ -44,6 +62,5 @@ export default defineConfig({
             formats: ["image/avif"],
             domains: ["creativehive.auxcgen.com"],
         },
-        functionPerRoute: false
     }),
 });
