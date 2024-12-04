76debf3cfc694b08f3652fb497ac7e59f229a094
Afham <noefeah89@gmail.com>
2024-12-04 19:56:06 +0530

    fix1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import robotsTxt from 'astro-robots-txt';
 import sitemap from 'astro-sitemap';
-import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 // import icon from "astro-icon";
 
 // import swup from '@swup/astro';
@@ -53,8 +52,7 @@ export default defineConfig({
     },
 
     output: 'server',
-    adapter: vercelServerless({
-        maxDuration: 60,
+    adapter: vercel({
         imageService: true,
         isr: {
             expiration: 3600,
@@ -64,6 +62,5 @@ export default defineConfig({
             formats: ["image/avif"],
             domains: ["creativehive.auxcgen.com"],
         },
-        runtime: 'nodejs20.x'
     }),
 });
